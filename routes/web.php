38b9d6550f42f03e5bd7f3685024a2a69184283c
nietzid <arfiansyah@upi.edu>
2022-08-03 14:51:38 +0700

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/cbt', [App\Http\Controllers\Cbt::class, 'index'])->name('home');
+
+Route::get('/cbt/create-test', [App\Http\Controllers\Cbt::class, 'createTest'])->name('home');
+
+Route::get('/cbt/start-test', [App\Http\Controllers\Cbt::class, 'startTest'])->name('home');
+
+Route::get('/cbt/test-landing-page', [App\Http\Controllers\Cbt::class, 'testLandingPage'])->name('home');
+
+Route::get('/cbt/test-result', [App\Http\Controllers\Cbt::class, 'testResult'])->name('home');
+
