11eac0e5702b9ae03fe10da5e97deac44fcf8eac
nietzid <arfiansyah@upi.edu>
2022-08-09 13:20:56 +0700

    Update Migration

---
+++ database/migrations/2022_08_08_050413_create_test_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_collections', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("description")->nullable();
+            $table->date('date');
+            $table->time('start_time', $precision = 0);
+            $table->time('end_time', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_collections');
+    }
+};
