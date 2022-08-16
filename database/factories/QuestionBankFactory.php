9a17c649d272012d244cb5138c3ce8a3e47aa024
nietzid <arfiansyah@upi.edu>
2022-08-16 12:31:49 +0700

    Update Question Bank Factory

--- database/factories/QuestionBankFactory.php
+++ database/factories/QuestionBankFactory.php
@@ -18,10 +18,12 @@ class QuestionBankFactory extends Factory
     public function definition()
     {
         $passagesIDs= DB::table('passages')->pluck('id');
+        $category=["Summary","Short Answer Question","Fill in The Blank","True or False","Multiple Choice"];
         return [
             'question' => fake()->sentence(),
             'answer' => fake()->sentence(), 
             'passage_id' => fake()->randomElement($passagesIDs),
+            'category'=> fake()->randomElement($category),
         ];
     }
 }
--- database/migrations/2022_08_08_051943_create_question_banks_table.php
+++ database/migrations/2022_08_08_051943_create_question_banks_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
                 ->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
+            $table->string("category");
         });
     }
 
