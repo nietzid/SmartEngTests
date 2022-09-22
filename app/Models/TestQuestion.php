9286f40f2a9ed8724e1a5f597d3671fecbee674e
nietzid <arfiansyah@upi.edu>
2022-09-22 11:35:54 +0700

    Backend Add Question to Test

--- app/Models/TestQuestion.php
+++ app/Models/TestQuestion.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TestQuestion extends Model{
+    protected $fillable = ['question_id','test_id'];
     public $timestamps = false;
     use HasFactory;
 }
