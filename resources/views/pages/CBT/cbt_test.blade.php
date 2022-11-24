c845f577b387a272bfd276f9ee7f32cb3faf8825
nietzid <arfiansyah@upi.edu>
2022-11-24 12:11:55 +0700

    Update CBT

--- resources/views/pages/CBT/cbt_test.blade.php
+++ resources/views/pages/CBT/cbt_test.blade.php
@@ -2,15 +2,21 @@
 
 @section('content')
 <div class="p-5 pb-3 font-poppins">
-
     <p id="demo" style="color: #3e6d81; font-size: 20px; font-weight: 600; text-align: right;"></p>
     <div class="row mb-4 mt-2 gap-4">
+        @php
+            // dd($questions);
+            $question = $questions[$id-1];
+        @endphp
         <div class="col-lg-9 col-md-9 col-12 p-4" style="text-align: justify; box-shadow: 0px 0px 10px -2px rgba(0, 0, 0, 0.35);">
-            <p>
-                
-            </p>
+            {{-- <?=dd($question)?> --}}
+            {{$question->passages}}
+            <br>
+            {{$question->question}}
+            <br>
             <div class="form-check">
                 <input type="radio" class="form-check-input" name="option" value="A" />
+                {{$question->answer}} 
                 <label class="form-check-label">
                 </label>
             </div>
@@ -29,24 +35,40 @@
                 <label class="form-check-label">
                 </label>
             </div>
+            
             <div class="row mt-3" style="float: right">
-                <a href="<?= url('cbt/test-result'); ?>">
-                    <button class="btn" style="background-color: #3e6d81; width: 150px; color: white;">
-                        Next
-                    </button>
+                <?php
+                    $total_question = $questions->count();
+                    if($id!=$total_question){
+                ?>
+                <a href="#"> //TODO
+                <button class="btn" style="background-color: #3e6d81; width: 150px; color: white;">
+                    Next
+                </button>
                 </a>
+                <?php
+                    }
+                    else{
+                ?>
+                <a href="<?php echo $id+1 ?>">
+                <button class="btn" style="background-color: #3e6d81; width: 150px; color: white;">
+                    Submit
+                </button>
+                </a>
+                <?php
+                    }
+                ?>
             </div>
         </div>
         <div class="col-lg col-md col p-4" style="background-color: white;text-align: justify; box-shadow: 0px 0px 10px -2px rgba(0, 0, 0, 0.35);">
             <!-- <div class="row row-cols-5 gap-3" style="text-align: center;">
                 <a style="background-color: lightgray;" class="page-link">
             </div> -->
-            <?php
-            $total_question = 10;
-            ?>
+            
+
             <div class="row row-cols-5 gap-3" style="text-align: center;">
                 <?php for ($x = 1; $x <= $total_question; $x++) { ?>
-                    <a style="background-color: lightgray;" class="page-link" href="?question=<?php echo $x ?>">
+                    <a style="background-color: lightgray;" class="page-link" href="<?php echo $x ?>">
                         <?php echo $x; ?>
                     </a>
                 <?php } ?>
@@ -55,8 +77,9 @@
     </div>
 </div>
 <script>
+    const langs = {{ Js::from(Language::all()) }};
     // Set the date we're counting down to
-    var countDownDate = new Date("September 22, 2022 17:00:00").getTime();
+    var countDownDate = new Date(question", 2022 17:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
