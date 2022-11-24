d11cd7db37bab2d906197bc7190216c236a63449
iraafy <irafitriyani62@gmail.com>
2022-11-24 10:49:48 +0700

    update edit page

--- resources/views/pages/Generate/detail_collection.blade.php
+++ resources/views/pages/Generate/detail_collection.blade.php
@@ -32,12 +32,79 @@
         <div class="row mt-4">
             <div class="col">
                 <span>
-                    <a href="/edit-question/{{$question->id}}">
-                        <button type="button btn" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" style="background-color: #3E6D81;">
-                            <iconify-icon inline icon="akar-icons:edit" style="color: white; font-size: 17px;"></iconify-icon>
-                        </button>
-                    </a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal-{{$question->id}}" style="background-color: #3E6D81;">
+                        <iconify-icon inline icon="akar-icons:edit" style="color: white; font-size: 17px;"></iconify-icon>
+                    </button>
                 </span>
+                <!-- Button trigger modal -->
+                <!-- <a href="detail.php?id_siswa=" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#exampleModal-">Detail</a> -->
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal-{{$question->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Detail Info</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body" style="text-align: justify">
+                                <table class="table">
+                                    <tr>
+                                        <td>
+                                            Passage
+                                        </td>
+                                        <td>
+                                            <input type="text" class="form-control W-100" value="{{$question->passages}}" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Option A
+                                        </td>
+                                        <td>
+                                            <input type="text" class="form-control" value="{{$question->question}}" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Option B
+                                        </td>
+                                        <td>
+                                            <input type="text" class="form-control" value="{{$question->question}}" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Option C
+                                        </td>
+                                        <td>
+                                            <input type="text" class="form-control" value="{{$question->question}}" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Option D
+                                        </td>
+                                        <td>
+                                            <input type="text" class="form-control" value="{{$question->question}}" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Answer
+                                        </td>
+                                        <td>
+                                            <input type="text" class="form-control" value="{{$question->an}}" />
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-secondary">Update</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <span>
                     <a href="/remove-question/{{$question->id}}">
                         <button class="btn" style="background-color: #CA6035;">
@@ -50,4 +117,13 @@
     </div>
     @endforeach
 </div>
+
+<script>
+    var myModal = document.getElementById('myModal')
+    var myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', function() {
+        myInput.focus()
+    })
+</script>
 @endsection
